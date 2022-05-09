e68a614c28fb74ae632517c3ff5d0d9ba3aeb583
Joseph-dh-woods <74608884+Joseph-dh-woods@users.noreply.github.com>
2022-05-09 03:31:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ keywords = ["game", "company", "cheats", "web", "http", "database",
             "scraper", "data", "api", "package"]
 setup(
     name='gamefaqs-py',
-    version='0.2.9',
+    version='0.2.10',
     author='Ryan Tonini',
     author_email ='ryantonini@yahoo.com',
     packages=['gamefaqs'],
